fd2a1d8ba953a8b41d88b652002177ce29f38899
lilachaimo <lilachaimo@gmail.com>
2020-06-25 23:46:13 -0400

    mvn.sh test change

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -11,6 +11,6 @@ echo "*********************************************"
 
 
 
-WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/hello
 
 docker run --rm  -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
