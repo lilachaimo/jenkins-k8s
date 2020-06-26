32e8cd0ea1165fc1f2c410883806b2d0fd551b68
lilachaimo <lilachaimo@gmail.com>
2020-06-25 23:43:06 -0400

    change mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -9,5 +9,5 @@ echo "*********************************************"
 echo "************ Building jar *******************"
 echo "*********************************************"
 
-WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/hello
 docker run --rm  -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
